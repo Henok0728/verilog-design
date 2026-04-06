28d51ed7d74db2383a72eacd2f25632cf5579f2d
henok7742 <henokgizaw06@gmail.com>
2026-04-06 21:25:59 +0300

    electronics

---
+++ xorgate/exorgate_tb.v
@@ -0,0 +1,23 @@
+module exorgate_tb;
+reg a;
+reg b;
+wire c;
+exorgate u0_dut(
+    .a_i(a),
+    .b_i(b),
+    .c_o(c)
+);
+initial begin
+
+$dumpfile("test_exor.vcd");
+$dumpvars(0,exorgate_tb);
+
+#1 a=1'b0;
+#4 a=1'b1;
+#5 a=1'b1;
+
+#200 $finish;
+end
+
+
+endmodule
