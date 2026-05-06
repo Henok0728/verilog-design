02aabd91665d0790814baed109b252372e30bd3f
henok7742 <henokgizaw06@gmail.com>
2026-05-06 23:59:50 +0300

    progres s on them

--- comb_circuits/assignment/decoder/fourtoninedecoder.v
+++ comb_circuits/assignment/decoder/fourtoninedecoder.v
@@ -1,14 +1,23 @@
 module fourtoninedecoder(
-    input [3:0] in,
+    input  [3:0] in,
     output reg [8:0] out
 );
-if (in == 4'b0000) out = 9'b000000001;
-else if (in == 4'b0001) out = 9'b000000010;
-else if (in == 4'b0010) out = 9'b000000100;
-else if (in == 4'b0011) out = 9'b000001000;
-else if (in == 4'b0100) out = 9'b000010000;
-else if (in == 4'b0101) out = 9'b000100000;
-else if (in == 4'b0110) out = 9'b001000000;
-else if (in == 4'b0111) out = 9'b010000000;
-else if (in == 4'b1000) out = 9'b100000000;
+
+    always @(*) begin
+        case (in)
+            
+            4'b1001:    out = 9'b100000000;
+            4'b1000:    out = 9'b010000000;
+            4'b0111:    out = 9'b001000000;
+            4'b0110:    out = 9'b000100000;
+            4'b0101:    out = 9'b000010000;
+            4'b0100:    out = 9'b000001000;
+            4'b0011:    out = 9'b000000100;
+            4'b0010:    out = 9'b000000010; 
+            4'b0001:    out = 9'b000000001; 
+            4'b0000:    out = 9'b000000000;
+            default: out = 9'b000000000;
+        endcase
+    end
+
 endmodule
--- comb_circuits/assignment/decoder/ninetofourencoder.v
+++ comb_circuits/assignment/decoder/ninetofourencoder.v
@@ -2,13 +2,18 @@ module ninetofourencoder(
     input [8:0] in,
     output reg [3:0] out
 );
-if (in[0]) out = 4'b0000;
-else if (in[1]) out = 4'b0001;
-else if (in[2]) out = 4'b0010;
-else if (in[3]) out = 4'b0011;
-else if (in[4]) out = 4'b0100;
-else if (in[5]) out = 4'b0101;
-else if (in[6]) out = 4'b0110;
-else if (in[7]) out = 4'b0111;
-else if (in[8]) out = 4'b1000;
+
+    always @(*) begin
+        if      (in[8]) out = 4'd9;
+        else if (in[7]) out = 4'd8;
+        else if (in[6]) out = 4'd7;
+        else if (in[5]) out = 4'd6;
+        else if (in[4]) out = 4'd5;
+        else if (in[3]) out = 4'd4;
+        else if (in[2]) out = 4'd3;
+        else if (in[1]) out = 4'd2;
+        else if (in[0]) out = 4'd1;
+        else            out = 4'd0;
+    end
+
 endmodule
