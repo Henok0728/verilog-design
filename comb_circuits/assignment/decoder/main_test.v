0331c4a7bb4afb1380dcf50739845b0b7e6fea23
henok7742 <henokgizaw06@gmail.com>
2026-05-07 01:05:54 +0300

    finished the upload of encoder-decoder-logic

--- comb_circuits/assignment/decoder/main_test.v
+++ comb_circuits/assignment/decoder/main_test.v
@@ -1,7 +1,7 @@
 module main_test;
 reg [3:0] in;
-reg [8:0] in2;
-wire [8:0] out;
+reg [9:0] in2;
+wire [9:0] out;
 wire [3:0] out2;
  
 fourtoninedecoder d1(.in(in),
@@ -12,17 +12,11 @@ ninetofourencoder e1(.in(in2),
                         );
 initial begin
     in = 4'b0000;#10; // Wait 10ns
-    in2 = 9'b000000000;
+    in2 = 10'b0000000000;
+    $display("check for update");
     #1; $display("in: %b, out: %b, in2: %b, out2: %b", in, out, in2, out2);
-    #10 in = 4'b0000; in2 = 9'b000000001;$display("in: %b, out: %b, in2: %b, out2: %b", in, out, in2, out2);
-    #10 in = 4'b0001; in2 = 9'b000000010;$display("in: %b, out: %b, in2: %b, out2: %b", in, out, in2, out2);
-    #10 in = 4'b0010; in2 = 9'b000000100;$display("in: %b, out: %b, in2: %b, out2: %b", in, out, in2, out2);
-    #10 in = 4'b0011; in2 = 9'b000001000;$display("in: %b, out: %b, in2: %b, out2: %b", in, out, in2, out2);
-    #10 in = 4'b0100; in2 = 9'b000010000;$display("in: %b, out: %b, in2: %b, out2: %b", in, out, in2, out2);
-    #10 in = 4'b0101; in2 = 9'b000100000;$display("in: %b, out: %b, in2: %b, out2: %b", in, out, in2, out2);
-    #10 in = 4'b0110; in2 = 9'b001000000;$display("in: %b, out: %b, in2: %b, out2: %b", in, out, in2, out2);
-    #10 in = 4'b0111; in2 = 9'b010000000;$display("in: %b, out: %b, in2: %b, out2: %b", in, out, in2, out2);
-    #10 in = 4'b1000; in2 = 9'b100000000;$display("in: %b, out: %b, in2: %b, out2: %b", in, out, in2, out2);
+    #10;in=4'b1001; in2 = 10'b1000000000;
+    #1; $display("in: %b, out: %b, in2': %b, out2: %b", in, out, in2, out2);
     $finish;
     end
 endmodule
--- comb_circuits/assignment/decoder/ninetofourencoder.v
+++ comb_circuits/assignment/decoder/ninetofourencoder.v
@@ -1,18 +1,20 @@
 module ninetofourencoder(
-    input [8:0] in,
+    input [9:0] in,
     output reg [3:0] out
 );
 
     always @(*) begin
-        if      (in[8]) out = 4'd9;
-        else if (in[7]) out = 4'd8;
-        else if (in[6]) out = 4'd7;
-        else if (in[5]) out = 4'd6;
-        else if (in[4]) out = 4'd5;
-        else if (in[3]) out = 4'd4;
-        else if (in[2]) out = 4'd3;
-        else if (in[1]) out = 4'd2;
-        else if (in[0]) out = 4'd1;
+
+        if      (in[9]) out = 4'd9;
+        else if (in[8]) out = 4'd8;
+        else if (in[7]) out = 4'd7;
+        else if (in[6]) out = 4'd6;
+        else if (in[5]) out = 4'd5;
+        else if (in[4]) out = 4'd4;
+        else if (in[3]) out = 4'd3;
+        else if (in[2]) out = 4'd2;
+        else if (in[1]) out = 4'd1;
+        else if (in[0]) out = 4'd0;
         else            out = 4'd0;
     end
 
