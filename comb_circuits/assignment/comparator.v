d2d5289ec5486a17eb8776dc2a5b1c6d30dfc50a
henok7742 <henokgizaw06@gmail.com>
2026-05-08 04:48:26 +0300

    test some

--- comb_circuits/assignment/comparator.v
+++ comb_circuits/assignment/comparator.v
@@ -1,19 +1,19 @@
 module comparator(input [3:0] a,b, 
-                  output reg gt, eq, lt);
+                  output gt,eq,lt);
+    wire [3:0] diff;
+    wire cout;
+    fourbit_adder_subtractor sub1(
+        .a(a),
+        .b(b),
+        .control(1'b1),
+        .result(diff),
+        .carry_out(cout)
+    );
 
-    always @(*) begin
-        if (a > b) begin
-            gt = 1;
-            eq = 0;
-            lt = 0;
-        end else if (a == b) begin
-            gt = 0;
-            eq = 1;
-            lt = 0;
-        end else begin
-            gt = 0;
-            eq = 0;
-            lt = 1;
-        end
-    end
+
+   assign eq = (diff == 4'b0000);
+   assign lt = ~(cout);
+   assign gt = cout & (~eq);
+
+
 endmodule
