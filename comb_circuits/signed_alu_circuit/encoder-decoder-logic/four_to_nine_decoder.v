5e8d638777a570221571c70b6f453110e4fe296e
henok7742 <henokgizaw06@gmail.com>
2026-05-09 23:21:41 +0300

    full complete embedded circuit for adder, subtractor and comparator circuit

--- comb_circuits/signed_alu_circuit/encoder-decoder-logic/four_to_nine_decoder.v
+++
@@ -1,5 +0,0 @@
-module four_o_nine_decoder(
-    input [3:0] in,
-    output reg [8:0] out
-);
-out =
--- comb_circuits/signed_alu_circuit/fourbits_adder_subtractor.v
+++ comb_circuits/signed_alu_circuit/fourbits_adder_subtractor.v
@@ -2,7 +2,7 @@ module fourbits_adder_subtractor(
     input [3:0] a,b,
     input  control, 
     output [3:0] result, 
-    output carry_out,negative,zero,overflow
+    output overflow
     );
     wire c1, c2, c3,c4;
     wire [3:0] sum,b_mode;
@@ -13,10 +13,5 @@ module fourbits_adder_subtractor(
     full_adder fa2(a[2], b_mode[2], c2, sum[2], c3);
     full_adder fa3(a[3], b_mode[3], c3, sum[3], c4);
     assign overflow = c3 ^ c4;
-    assign result = (overflow) ? 4'b0000 : sum;
-    assign carry_out = c4 ^ control;
-    assign zero = ~|result;
-   
-    assign negative = result[3];
-
+    assign result = (overflow) ? 4'b1001 : sum;
 endmodule
